539d0fc1a744a5f99753e8c1c3565016d1c32f1e
Xeraph <jeongdo.dev@gmail.com>
2024-05-28 13:21:13 +0900

    BST 노드 삭제 메서드 테스트

--- src/BinarySearchTree/BST.test.ts
+++ src/BinarySearchTree/BST.test.ts
@@ -1,4 +1,4 @@
-import BST, { BSTNode } from "./BST";
+import BST from "./BST";
 
 describe("BinarySearchTree", () => {
   let tree: BST;
@@ -7,51 +7,110 @@ describe("BinarySearchTree", () => {
     tree = new BST();
   });
 
-  test("should insert nodes correctly", () => {
-    tree.insert(10);
-    tree.insert(8);
-    tree.insert(5);
-    tree.insert(12);
-    tree.insert(14);
+  test("insert", () => {
+    tree.insert(50);
+    tree.insert(35);
+    tree.insert(55);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(52);
+    tree.insert(60);
+    tree.insert(15);
+    tree.insert(34);
+    tree.insert(38);
+    tree.insert(51);
+    tree.insert(53);
 
     const root = tree.root;
-    expect(root?.data).toBe(10);
-    expect(root?.left?.data).toBe(8);
-    expect(root?.left?.left?.data).toBe(5);
-    expect(root?.right?.data).toBe(12);
-    expect(root?.right?.right?.data).toBe(14);
+    expect(root?.data).toBe(50);
+    expect(root?.left?.data).toBe(35);
+    expect(root?.left?.left?.data).toBe(30);
+    expect(root?.right?.data).toBe(55);
+    expect(root?.right?.right?.data).toBe(60);
   });
 
-  test("should perform pre-order traversal correctly", () => {
-    tree.insert(10);
-    tree.insert(8);
-    tree.insert(5);
-    tree.insert(12);
-    tree.insert(14);
+  test("insert & preOrder", () => {
+    tree.insert(50);
+    tree.insert(35);
+    tree.insert(55);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(52);
+    tree.insert(60);
+    tree.insert(15);
+    tree.insert(34);
+    tree.insert(38);
+    tree.insert(51);
+    tree.insert(53);
 
     const result = tree.preOrder(tree.root);
-    expect(result).toEqual([10, 8, 5, 12, 14]);
+    expect(result).toEqual([50, 35, 30, 15, 34, 40, 38, 55, 52, 51, 53, 60]);
   });
 
-  test("should perform in-order traversal correctly", () => {
-    tree.insert(10);
-    tree.insert(8);
-    tree.insert(5);
-    tree.insert(12);
-    tree.insert(14);
+  test("insert & inOrder", () => {
+    tree.insert(50);
+    tree.insert(35);
+    tree.insert(55);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(52);
+    tree.insert(60);
+    tree.insert(15);
+    tree.insert(34);
+    tree.insert(38);
+    tree.insert(51);
+    tree.insert(53);
 
     const result = tree.inOrder(tree.root);
-    expect(result).toEqual([5, 8, 10, 12, 14]);
+    expect(result).toEqual([15, 30, 34, 35, 38, 40, 50, 51, 52, 53, 55, 60]);
   });
 
-  test("should perform post-order traversal correctly", () => {
-    tree.insert(10);
-    tree.insert(8);
-    tree.insert(5);
-    tree.insert(12);
-    tree.insert(14);
+  test("insert & postOrder", () => {
+    tree.insert(50);
+    tree.insert(35);
+    tree.insert(55);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(52);
+    tree.insert(60);
+    tree.insert(15);
+    tree.insert(34);
+    tree.insert(38);
+    tree.insert(51);
+    tree.insert(53);
 
     const result = tree.postOrder(tree.root);
-    expect(result).toEqual([5, 8, 14, 12, 10]);
+    expect(result).toEqual([15, 34, 30, 38, 40, 35, 51, 53, 52, 60, 55, 50]);
+  });
+
+  test("insert & delete & inOrder", () => {
+    tree.insert(50);
+    tree.insert(35);
+    tree.insert(55);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(52);
+    tree.insert(60);
+    tree.insert(15);
+    tree.insert(34);
+    tree.insert(38);
+    tree.insert(51);
+    tree.insert(53);
+
+    tree.delete(55);
+    let result = tree.inOrder(tree.root);
+    expect(result).toEqual([15, 30, 34, 35, 38, 40, 50, 51, 52, 53, 60]);
+
+    tree.delete(50);
+    result = tree.inOrder(tree.root);
+    expect(result).toEqual([15, 30, 34, 35, 38, 40, 51, 52, 53, 60]);
+
+    tree.delete(15);
+    result = tree.inOrder(tree.root);
+    expect(result).toEqual([30, 34, 35, 38, 40, 51, 52, 53, 60]);
+
+    tree.delete(53);
+    result = tree.inOrder(tree.root);
+    expect(result).toEqual([30, 34, 35, 38, 40, 51, 52, 60]);
   });
 });
--- src/BinarySearchTree/BST.ts
+++ src/BinarySearchTree/BST.ts
@@ -1,122 +1,3 @@
-// class BSTNode {
-//   data: number;
-//   left: BSTNode | null;
-//   right: BSTNode | null;
-//   constructor(data: number) {
-//     this.data = data;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
-
-// class BST {
-//   root: BSTNode | null;
-//   constructor() {
-//     this.root = null;
-//   }
-
-//   finMaxNode(node: BSTNode) {
-//     let _node = node;
-//     while (_node.right !== null) {
-//       _node = _node.right;
-//     }
-//     return _node;
-//   }
-
-//   remove(data: number) {
-//     this.root = this.removeNode(this.root, data);
-//   }
-
-//   removeNode(node: BSTNode | null, key: number) {
-//     if (node === null) {
-//       return null;
-//     }
-//     // 노드보다 작을 경우, 왼쪽
-//     if (key < node.data) {
-//       node.left = this.removeNode(node.left, key);
-//       return node;
-//     }
-//     // 노드보다 클 경우, 오른쪽
-//     if (key > node.data) {
-//       node.right = this.removeNode(node.right, key);
-//       return node;
-//     }
-//     // 타겟 노드일 경우
-//     // case1) left, right node 없을 경우
-//     if (node.left === null && node.right === null) {
-//       node = null;
-//       return node;
-//     }
-//     // case2) left or right node 둘 중 하나만 있을 경우
-//     // right node 있을 경우
-//     if (node.left === null) {
-//       node = node.right;
-//       return node;
-//     }
-//     // left node 있을 경우
-//     if (node.right === null) {
-//       node = node.left;
-//       return node;
-//     }
-
-//     // case3) left와 right node 둘다 있을 경우.
-//     const leftSubTreeMinNode = this.finMaxNode(node.left);
-//     node.data = leftSubTreeMinNode.data;
-//     node.left = this.removeNode(node.left, leftSubTreeMinNode.data);
-//     return node;
-//   }
-
-//   preOrder(node: BSTNode | null, result: number[] = []) {
-//     if (node !== null) {
-//       result.push(node.data);
-//       this.preOrder(node.left, result);
-//       this.preOrder(node.right, result);
-//     }
-//     return result;
-//   }
-
-//   insert(data: number) {
-//     const newNode = new BSTNode(data);
-//     if (this.root === null) {
-//       this.root = newNode;
-//       return;
-//     } else this.insertNode(this.root, newNode);
-//   }
-
-//   insertNode(currentNode: BSTNode, newNode: BSTNode) {
-//     // 비교 노드가 더 클 경우, 왼쪽으로
-//     if (currentNode.data > newNode.data) {
-//       // 왼쪽 비어있을 경우 삽입
-//       if (currentNode.left === null) {
-//         currentNode.left = newNode;
-//         return;
-//       } else {
-//         this.insertNode(currentNode.left, newNode);
-//       }
-//       // 비교 노드가 더 작을 경우, 오른쪽으로
-//     } else if (currentNode.data < newNode.data) {
-//       if (currentNode.right === null) {
-//         currentNode.right = newNode;
-//         return;
-//       } else {
-//         this.insertNode(currentNode.right, newNode);
-//       }
-//     } else {
-//       return;
-//     }
-//   }
-// }
-
-// const BST_TEST = new BST();
-// BST_TEST.insert(10);
-// BST_TEST.insert(6);
-// BST_TEST.insert(12);
-// BST_TEST.insert(15);
-// BST_TEST.insert(8);
-// BST_TEST.remove(6);
-
-// console.log(BST_TEST.preOrder(BST_TEST.root, []));
-
 class BSTNode {
   data: number;
   left: BSTNode | null;
@@ -192,8 +73,58 @@ class BST {
       }
       // 이미 값이 존재하는 경우
     } else {
-      return console.log("이미 값이 존재합니다.");
+      return "이미 값이 존재합니다.";
     }
+  }
+
+  findMaxNode(node: BSTNode) {
+    let maxNode = node;
+    while (maxNode.right !== null) {
+      maxNode = maxNode.right;
+    }
+    return maxNode;
+  }
+
+  delete(target: number) {
+    this.root = this.deleteNode(this.root, target);
+  }
+
+  deleteNode(node: BSTNode | null, target: number) {
+    if (node === null) {
+      return null;
+    }
+    // 비교노드 < target, 오른쪽 이동
+    if (node.data < target) {
+      node.right = this.deleteNode(node.right, target);
+      return node;
+    }
+    // 비교노드 > target, 왼쪽 이동
+    if (node.data > target) {
+      node.left = this.deleteNode(node.left, target);
+      return node;
+    }
+    // node.data === target
+    // case1 left, right node 없을 경우
+    if (node.left === null && node.right === null) {
+      node = null;
+      return node;
+    }
+
+    // case2 left or right 둘 중 한 곳에 node가 있을 경우
+    if (node.left === null) {
+      node = node.right;
+      return node;
+    }
+    if (node.right === null) {
+      node = node.left;
+      return node;
+    }
+
+    // case3 left & right 모두에 node가 있을 경우
+    const leftSubTreeMaxNode = this.findMaxNode(node.left);
+    node.data = leftSubTreeMaxNode.data;
+    node.left = this.deleteNode(node.left, leftSubTreeMaxNode.data);
+    return node;
   }
 }
 
@@ -203,7 +134,7 @@ BST_TEST.insert(8);
 BST_TEST.insert(5);
 BST_TEST.insert(12);
 BST_TEST.insert(14);
-
+BST_TEST.delete(12);
 BST_TEST.preOrder(BST_TEST.root);
 
 export default BST;
